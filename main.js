d3c9d20282e3c54dcf3cba3ba1c39dfa0f934756
phillip Eauclair <54328450+PhillipEauclair@users.noreply.github.com>
2019-11-04 08:37:57 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,17 +7,7 @@ const getUserChoice = userInput => {
   }
 }
 
-function getComputerChoice() {
-  
-  switch (Math.floor(Math.random() * 3)) {
-    case 0:
-      return 'rock';
-    case 1:
-      return 'paper';
-    case 2:
-      return 'scissors'
-  }
-}
+
 
 function determineWinner(userChoice, computerChoice) {
 if (userChoice === computerChoice) {
